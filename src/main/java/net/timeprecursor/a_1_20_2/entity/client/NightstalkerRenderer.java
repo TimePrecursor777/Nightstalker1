74db8ccef0f38dc618c4756c38e7c9a170e2daf6
Time <shannonstirling87@gmail.com>
2024-03-23 08:57:07 +1300

    first

---
+++ src/main/java/net/timeprecursor/a_1_20_2/entity/client/NightstalkerRenderer.java
@@ -0,0 +1,4 @@
+package net.timeprecursor.a_1_20_2.entity.client;
+
+public class NightstalkerRenderer {
+}
---
+++ src/main/java/net/timeprecursor/a_1_20_2/entity/custom/NightstalkerEntity.java
@@ -0,0 +1,4 @@
+package net.timeprecursor.a_1_20_2.entity.custom;
+
+public class NightstalkerEntity {
+}
